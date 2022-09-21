b67f2ae1f28e625a2c8bc96ac3dae2dae08b0673
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-09-21 16:34:10 -0300

    Feat: Front-end - Funções de validação da tela login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import Router from './Router';
 import Provider from './context/Provider';
 
@@ -8,12 +7,6 @@ function App() {
   return (
     // <>
     <Provider>
-      <div className="App">
-        <span className="logo">TRYBE</span>
-        <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-          Glass
-        </object>
-      </div>
       <Router />
     </Provider>
     // </>
