c84120449dff68b8fd46f8e75d5e8dd25c5958a8
Patricia Padilha <patriciapadilhabio@gmail.com>
2022-09-22 14:46:33 -0300

    feat: Css Tela Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { validate } from 'email-validator';
 import Context from '../context/Context';
+import '../css/Login.css';
 
 function Login() {
   const prefix = 'common_login__';
@@ -35,39 +36,46 @@ function Login() {
       { console.log(email) }
       { console.log(password) }
       { console.log(ableBtn) }
-      <main>
-        <label htmlFor="email-input">
-          <h2>Login</h2>
-          <input
-            type="email"
-            id="email-input"
-            data-testid={ `${prefix}input-email` }
-            onChange={ (event) => setEmail(event.target.value) }
-          />
-        </label>
+      <main className="block-main">
+        <form>
+          <label htmlFor="email-input">
+            <h2>Login</h2>
+            <input
+              type="email"
+              id="email-input"
+              data-testid={ `${prefix}input-email` }
+              onChange={ (event) => setEmail(event.target.value) }
+            />
+          </label>
 
-        <label htmlFor="password-input">
-          <h2>Senha</h2>
-          <input
-            type="password"
-            id="password-input"
-            data-testid={ `${prefix}input-password` }
-            onChange={ (event) => setPassword(event.target.value) }
-          />
-        </label>
-        <button
-          disabled={ ableBtn }
-          type="submit"
-          data-testid={ `${prefix}button-login` }
-        >
-          LOGIN
-        </button>
-
-        <Link to="/register">
-          <button type="button" data-testid={ `${prefix}button-register` }>
-            Ainda não tem conta
+          <label htmlFor="password-input">
+            <h2>Senha</h2>
+            <input
+              type="password"
+              id="password-input"
+              data-testid={ `${prefix}input-password` }
+              onChange={ (event) => setPassword(event.target.value) }
+            />
+          </label>
+          <button
+            className="btn-green"
+            disabled={ ableBtn }
+            type="submit"
+            data-testid={ `${prefix}button-login` }
+          >
+            LOGIN
           </button>
-        </Link>
+
+          <Link to="/register">
+            <button
+              className="btn-gray"
+              type="button"
+              data-testid={ `${prefix}button-register` }
+            >
+              Ainda não tem conta
+            </button>
+          </Link>
+        </form>
 
         <p
           data-testid={ `${prefix}element-invalid-email` }
