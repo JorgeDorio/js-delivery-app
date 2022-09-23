136d2cf584e43a8cee8cb5119e5358897cd11669
Leandro O. Bastos <obastosleandro@gmail.com>
2022-09-23 14:16:00 -0300

    feat: front-end css da tela de login e register

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { validate } from 'email-validator';
 import Context from '../context/Context';
+import '../css/Register.css';
 
 function Register() {
   const prefix = 'common_register__';
@@ -37,52 +38,58 @@ function Register() {
       { console.log(email) }
       { console.log(password) }
       <title>Cadastro</title>
-      <main>
+      <main className="register-main">
+        <h1>Cadastro</h1>
+        <form>
 
-        <label htmlFor="name-input">
-          <h2>Nome</h2>
-          <input
-            type="name"
-            id="name-input"
-            data-testid={ `${prefix}input-name` }
-            onChange={ (event) => setName(event.target.value) }
-          />
-        </label>
 
-        <label htmlFor="email-input">
-          <h2>Login</h2>
-          <input
-            type="email"
-            id="email-input"
-            data-testid={ `${prefix}input-email` }
-            onChange={ (event) => setEmail(event.target.value) }
-          />
-        </label>
+          <label htmlFor="name-input">
+            <h2>Nome</h2>
+            <input
+              placeholder="  Seu nome"
+              type="name"
+              id="name-input"
+              data-testid={ `${prefix}input-name` }
+              onChange={ (event) => setName(event.target.value) }
+            />
+          </label>
 
-        <label htmlFor="password-input">
-          <h2>Senha</h2>
-          <input
-            type="password"
-            id="password-input"
-            data-testid={ `${prefix}input-password` }
-            onChange={ (event) => setPassword(event.target.value) }
-          />
-        </label>
+          <label htmlFor="email-input">
+            <h2>Email</h2>
+            <input
+              placeholder="  seu-email@site.com.br"
+              type="email"
+              id="email-input"
+              data-testid={ `${prefix}input-email` }
+              onChange={ (event) => setEmail(event.target.value) }
+            />
+          </label>
+
+          <label htmlFor="password-input">
+            <h2>Senha</h2>
+            <input
+              placeholder="  **********"
+              type="password"
+              id="password-input"
+              data-testid={ `${prefix}input-password` }
+              onChange={ (event) => setPassword(event.target.value) }
+            />
+          </label>
+          <button
+            className="btn-green"
+            type="submit"
+            data-testid={ `${prefix}button-register` }
+            disabled={ ableBtn }
+          >
+            CADASTRAR
+          </button>
+        </form>
+        <p
+          data-testid={ `${prefix}element-invalid-register` }
+        >
+          Elemento oculto (Mensagem de erro)
+        </p>
       </main>
-
-      <button
-        type="submit"
-        data-testid={ `${prefix}button-register` }
-        disabled={ ableBtn }
-      >
-        CADASTRAR
-      </button>
-
-      <p
-        data-testid={ `${prefix}element-invalid-register` }
-      >
-        Elemento oculto (Mensagem de erro)
-      </p>
     </>
   );
 }
