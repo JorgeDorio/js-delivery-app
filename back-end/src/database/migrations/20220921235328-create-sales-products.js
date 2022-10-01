4e479ea69439222056c2fb843d0cb9a84f7e9e0e
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-09-30 21:46:05 -0300

    Arrumando nome da tabela sales_products

--- back-end/src/database/migrations/20220921235328-create-sales-products.js
+++ back-end/src/database/migrations/20220921235328-create-sales-products.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('salesProducts', {
+    await queryInterface.createTable('sales_products', {
       sale_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -37,6 +37,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('salesProducts');
+    await queryInterface.dropTable('sales_products');
   }
 };
